cd139204288be416712684f3285a1a1933817f5b
Mauricio Núñez Jiménez <themjnunez@gmail.com>
2014-09-05 13:50:43 -0500

    Add pause/play and shuffle functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
 (function() {
-  var universe = [];
-  for (var i = 0; i < 100; i++) {
-    universe.push([]);
-    for (var j = 0; j < 100; j++) {
-      universe[i].push( Math.round(Math.random()));
-    }
+  var universe = [],
+      paused = true,
+      toggle = document.getElementById('toggle'),
+      timer;
+
+  function pause() {
+    clearInterval(timer);
+    toggle.innerHTML = 'play';
+    paused = true;
   }
 
-  setInterval(function() {
-    graphics.scene(universe);
+  function play() {
+    timer = setInterval(animate, 250);
+    toggle.innerHTML = 'pause';
+    paused = false;
+  }
+
+  function shuffle() {
+    var universe = [];
+    for (var i = 0; i < 100; i++) {
+      universe.push([]);
+      for (var j = 0; j < 70; j++) {
+        universe[i].push( Math.round(Math.random()));
+      }
+    }
+    return universe;
+  }
+
+  function animate() {
     universe = gameOfLife.tick(universe);
-  }, 250);
+    graphics.scene(universe);
+  }
+
+  function paint() {
+    universe = shuffle();
+    graphics.scene(universe);
+  }
+
+  paint()
+  toggle.addEventListener('click', function() {
+    if (paused) {
+      play();
+    } else {
+      pause();
+    }
+  });
+  document.getElementById('shuffle').addEventListener('click', function() {
+    if (!paused) {
+      pause();
+    }
+    paint();
+  });
 })();
